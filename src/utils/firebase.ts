f585a692f9e59937be05707cdc26c717f72651ad
giaunguyen2604 <giau.nguyen@executionlab.asia>
2024-07-08 09:33:05 +0700

    update config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -14,13 +14,13 @@ import {
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyCkoiD4ZSNUPYezoR7_EKB8LoayE9D5Ax0',
-	authDomain: 'image-gallery-a9ebb.firebaseapp.com',
-	projectId: 'image-gallery-a9ebb',
-	storageBucket: 'image-gallery-a9ebb.appspot.com',
-	messagingSenderId: '881723409546',
-	appId: '1:881723409546:web:bd1d7b62e1a2e32a145973',
-	measurementId: 'G-TFXCYLM1EB',
+	apiKey: 'AIzaSyBVAB43yQJ8wydm3FWiYpEzwEvl1cCyER8',
+	authDomain: 'fir-learning-38320.firebaseapp.com',
+	projectId: 'fir-learning-38320',
+	storageBucket: 'fir-learning-38320.appspot.com',
+	messagingSenderId: '286709041019',
+	appId: '1:286709041019:web:0751fc49e7d2c0761d9fde',
+	measurementId: 'G-DW49NBVYS9',
 };
 
 const app =
